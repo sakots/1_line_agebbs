45c583569a89141de61b6e99a7250f5e452a5a92
sakots <sakots8000@gmail.com>
2023-01-05 01:25:51 +0900

    リプライ

--- index.php
+++ index.php
@@ -9,6 +9,29 @@ define('DB_PDO', 'sqlite:age.db');
 //初期設定
 init();
 
+/*-----------mode-------------*/
+
+$mode = filter_input(INPUT_POST, 'mode');
+
+if (filter_input(INPUT_GET, 'mode') === "regist") {
+	$mode = "regist";
+}
+if (filter_input(INPUT_GET, 'mode') === "reply") {
+	$mode = "reply";
+}
+
+switch ($mode) {
+  case 'regist':
+    return regist();
+  case 'reply':
+    return reply();
+  default:
+    return def();
+}
+exit;
+
+/*-----------Main-------------*/
+
 function init() {
   try {
     if (!is_file('age.db')) {
@@ -55,6 +78,52 @@ function regist() {
   } catch (PDOException $e) {
 		echo "DB接続エラー:" . $e->getMessage();
 	}
+}
+
+//投稿をデータベースへ保存 - リプライ
+function reply() {
+  $name = (string)filter_input(INPUT_POST, 'name');
+  $com = (string)filter_input(INPUT_POST, 'com');
+  $parent = trim(filter_input(INPUT_POST, 'parent', FILTER_VALIDATE_INT));
+  try {
+    $db = new PDO(DB_PDO);
+    if (isset($_POST["send"])) {
+      if ($name === "") $name = '名無しさん';
+			if ($com === "") $com  = 'コメントなし';
+
+      //最新コメント取得
+			$sqlw = "SELECT * FROM tlog WHERE thread=0 ORDER BY tid DESC LIMIT 1";
+			$msgw = $db->prepare($sqlw);
+			$msgw->execute();
+			$msgwc = $msgw->fetch();
+
+      //最初のレスのage処理対策
+			$msgwc["tid"] = 0;
+			$msgwc["age"] = 0;
+			$msgwc["tree"] = 0;
+      
+      // 'のエスケープ
+      $name = str_replace("'", "''", $name);
+      $com = str_replace("'", "''", $com);
+
+      // レスの位置
+			$tree = time() - $parent - (int)$msgwc["tid"];
+			$comid = $tree + time();
+
+			$thread = 0;
+			$age = (int)$msgwc["age"];
+
+      $sql = "INSERT INTO tlog (created, thread, parent, comid, tree, a_name, com, age) VALUES (datetime('now', 'localtime'), '$thread', '$parent', '$comid', '$tree', '$name', '$com', '$age')";
+			$db->exec($sql);
+    }
+  } catch (PDOException $e) {
+		echo "DB接続エラー:" . $e->getMessage();
+	}
+}
+
+//通常表示モード
+function def() {
+
 }
 
 //エラー画面
