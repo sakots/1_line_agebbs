1425dbc9fc93e636d94d1d39f0f27300983316a7
sakots <sakots8000@gmail.com>
2023-01-05 00:33:58 +0900

    db作成まで

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//----------
+// 1行age機能付きbbsサンプル
+//----------
+
+//データベース接続PDO
+define('DB_PDO', 'sqlite:age.db');
+
+//初期設定
+init();
+
+function init() {
+  try {
+    if (!is_file('age.db')) {
+      // はじめての実行ならDB作成
+      // ID, スレ親orレス, 親スレ, コメントID, スレ構造ID, 名前, 本文
+      $db = new PDO(DB_PDO);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "CREATE TABLE tlog (tid integer primary key autoincrement, thread VARCHAR(1), parent INT, comid BIGINT, tree BIGINT, a_name TEXT, com TEXT)";
+			$db = $db->query($sql);
+			$db = null; //db切断
+    }
+  } catch (PDOException $e) {
+		echo "DB接続エラー:" . $e->getMessage();
+	}
+}
