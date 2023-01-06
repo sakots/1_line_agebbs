231798ab38a3cea0303e0858010413346f31250b
sakots <sakots8000@gmail.com>
2023-01-07 04:58:18 +0900

    style

--- template/main.blade.php
+++ template/main.blade.php
@@ -5,6 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>1行BBS</title>
+  <style>
+    .rep {margin:0;padding:0;}
+  </style>
 </head>
 <body>
   <h1>1行BBS age機能付き</h1>
@@ -27,12 +30,13 @@
       @if (!empty($ko))
 			@foreach ($ko as $res)
       @if ($bbsline['tid'] === $res['parent'])
-        <section>└ [{{$res['tid']}}] : {{$res['created']}} - {{$res['a_name']}} : {{$res['com']}}</section>
+        <section class="rep">└ [{{$res['tid']}}] : {{$res['created']}} - {{$res['a_name']}} : {{$res['com']}}</section>
       @endif
       @endforeach
       @endif
       <form action="index.php?mode=reply">
         <p>
+          リプライ -
           <label for="rep_a_name_{{$bbsline['tid']}}">名前：<input type="text" id="rep_a_name_{{$bbsline['tid']}}" name="name" size="10"></label>
           <label for="rep_com_{{$bbsline['tid']}}">本文：<input type="text" id="rep_com_{{$bbsline['tid']}}" name="com" size="20"></label>
           <input type="submit" id="submit" name="send" value="書き込む">
