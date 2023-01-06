62c6880cf038801bdc23ae05e5001ae112941782
sakots <sakots8000@gmail.com>
2023-01-07 04:55:05 +0900

    template

---
+++ template/error.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="1; URL=./">
+  <title>ERROR</title>
+</head>
+<body>
+  <h1>ERROR</h1>
+  <p>なんかエラーです。画面を切り替えます。</p>
+</body>
+</html>
