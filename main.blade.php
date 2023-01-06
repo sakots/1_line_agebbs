abe3b504c0847ef9e640f6d8570ba7cdfdfe38ce
sakots <sakots8000@gmail.com>
2023-01-07 04:20:57 +0900

    完成？

--- main.blade.php
+++ main.blade.php
@@ -12,12 +12,33 @@
   <h2>スレッド作成</h2>
   <form action="index.php?mode=regist" method="post" enctype="multipart/form-data">
     <p>
-      <label for="a_name">名前：<input type="text" id="a_name" name="name"></label>
-      <label for="com">本文：<input type="text" id="com" name="com"></label>
+      <label for="a_name">名前：<input type="text" id="a_name" name="name" size="10"></label>
+      <label for="com">本文：<input type="text" id="com" name="com" size="20"></label>
       <input type="submit" id="submit" name="send" value="書き込む">
     </p>
   </form>
   <hr>
-
+  <h2>本文</h2>
+  <div>
+    @if (!empty($oya))
+    @foreach ($oya as $bbsline)
+    <section class="thread">
+      [{{$bbsline['tid']}}] - {{$bbsline['a_name']}} : {{$bbsline['com']}}
+      @if (!empty($ko))
+			@foreach ($ko as $res)
+        <section>└ [{{$ko['tid']}}] - {{$ko['a_name']}} : {{$ko['com']}}</section>
+      @endforeach
+      @endif
+      <form action="index.php?mode=reply">
+        <p>
+          <label for="rep_a_name_{{$bbsline['tid']}}">名前：<input type="text" id="rep_a_name_{{$bbsline['tid']}}" name="name" size="10"></label>
+          <label for="rep_com_{{$bbsline['tid']}}">本文：<input type="text" id="rep_com_{{$bbsline['tid']}}" name="com" size="20"></label>
+          <input type="submit" id="submit" name="send" value="書き込む">
+        </p>
+      </form>
+    </section>
+    @endforeach
+    @endif
+  </div>
 </body>
 </html>
